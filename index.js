b19b4d333513d3b093a826a31fe4ed1119db95e2
Martín M <skydiver@users.noreply.github.com>
2019-02-17 03:08:49 -0300

    enable cors

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const mailgun = require('mailgun-js')({
 
 const template = require('./lib/template');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
